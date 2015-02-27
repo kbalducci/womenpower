c2de0a87b4d4f1e2b16357677f7d6d39638350c7
Erika Moller <erikamoller@gmail.com>
2015-02-27 13:04:28 -0600

    Updated Dashboard with Dawn's info

--- app/assets/javascripts/controllers/controllers.js
+++ app/assets/javascripts/controllers/controllers.js
@@ -1,15 +1,15 @@
 angular.module('app.controllers', [])
 .controller('DashboardCtrl', function($scope, $state) {
 	// $scope.test = 'hello';
-});
-// .controller('InspirationCtrl', function($scope, $state) {
-// 	$scope.test = 'hello';
-// })
+})
+.controller('InspirationCtrl', function($scope, $state) {
+	// $scope.test = 'hello';
+})
 // .controller('SettingsCtrl', function($scope, $state) {
-// 	$scope.test = 'hello';
-// 	$scope.goHome = function() {
-// 		console.log('goHome');
-// 		$state.go('home');
-// 	}
+// 	// $scope.test = 'hello';
+// 	// $scope.goHome = function() {
+// 	// 	console.log('goHome');
+// 	// 	$state.go('home');
+// 	// }
 // })
 // 
