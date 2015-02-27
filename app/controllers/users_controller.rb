5ff59e690c5d60e567064961a4131476c13e7a3b
Kate <kbalducci@hotmail.com>
2015-02-27 16:19:48 -0600

    updated devise and params

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,19 @@
 class UsersController < ApplicationController
-end
+protect_from_forgery with: :exception
+
+  def create
+  @user = User.new(user_params)
+
+    respond_to do |format|
+      if @user.save
+          format.json { render json: @user, status: :created, location: @user }
+      else
+          format.json { render json: @user.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  private
+    def user_params
+      params.require(:user).permit(:first_name, :last_name)
+    end
--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -2,15 +2,5 @@ class WelcomeController < ApplicationController
   def index
   end
 
-  def create
-  @user = User.new(user_params)
 
-  respond_to do |format|
-    if @user.save
-        format.json { render json: @user, status: :created, location: @user }
-    else
-        format.json { render json: @user.errors, status: :unprocessable_entity }
-    end
-  end
-end
 end
