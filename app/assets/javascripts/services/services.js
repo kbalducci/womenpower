be7b579cca767642fbfe60c734673f506498b533
Erika Moller <erikamoller@gmail.com>
2015-02-26 22:35:16 -0600

    Setup controller, app, services files and various html files

---
+++ app/assets/javascripts/services/services.js
@@ -0,0 +1,12 @@
+angular.module('basic.services', [])
+
+.factory('Random', function() {
+	return function(array) {
+		var computerChoice = Math.floor(Math.random() * array.length);
+		return array[computerChoice];
+	}
+	// var test = function(array) {
+	// 	...
+	// }
+	// return test;
+})
