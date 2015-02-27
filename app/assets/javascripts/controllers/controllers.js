41a54a419a80931c04d2726038ca2ffe2e99f34a
Erika Moller <erikamoller@gmail.com>
2015-02-27 17:52:56 -0600

    Testing pulling categories

--- app/assets/javascripts/controllers/controllers.js
+++ app/assets/javascripts/controllers/controllers.js
@@ -5,8 +5,67 @@ angular.module('app.controllers', [])
 .controller('InspirationCtrl', function($scope, $state) {
 	// $scope.test = 'hello';
 })
-.controller('CategoryCtrl', function($scope, $state) {
-	// $scope.test = 'hello';
+.controller('CategoryCtrl', function($scope, $state, $http) {
+	// $http.get(...)
+	// .success(function(categories) {
+		var categories = [
+			{
+				id: 1,
+				name: 'Student'
+			},
+			{
+				id: 2,
+				name: 'Professional'
+			},
+			{
+				id: 3,
+				name: 'Personal'
+			}
+		];
+
+		$scope.categories = categories;
+
+		// console.log($scope.categories.id);
+	// })
+
+	// $scope.student1 = true;
+	// $scope.issueOptions = false;
+
+	// $scope.studentBtn = function() {
+	// 	return $scope.issueOptions = true;
+	// }
+
+	$scope.studentBtn = function(id) {
+		console.log(id);
+
+		// var promise = $http.get('http://tiny-pizza-server.herokuapp.com/collections/emoller-ang')
+		// .success(function(response) {
+
+		// 	$scope.inspiration = [];
+		// 	for(var i=0; i<response.length; i++) {
+
+		// 		console.log(i);
+		// 		console.log(response[i]);
+		// 		console.log(response[i].image);
+		// 		console.log(response[i].name);
+		// 		console.log(response[i].quote);
+
+		// 			if(response[i].image && response[i].name && response[i].quote) {
+		// 				$scope.inspiration.push(response[i]);
+		// 				console.log($scope.inspiration);
+		// 			}
+		// 	}
+
+		// 	console.log(response);
+		// })
+
+		// .error(function(err) {
+		// 	console.log(err);
+		// })
+
+	}
+// issue1();
+	
 })
 // .controller('SettingsCtrl', function($scope, $state) {
 // 	// $scope.test = 'hello';
