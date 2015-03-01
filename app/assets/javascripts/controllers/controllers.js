d5e6b47fb5b8e526a952e1c6f1330d033c32f34d
Erika Moller <erikamoller@gmail.com>
2015-03-01 12:08:21 -0600

    Fixed incoming data from the server from repeating. Used show/hide to display data

--- app/assets/javascripts/controllers/controllers.js
+++ app/assets/javascripts/controllers/controllers.js
@@ -1,132 +1,58 @@
 angular.module('app.controllers', [])
 .controller('DashboardCtrl', function($scope, $state) {
-
-	
-
 })
 .controller('InspirationCtrl', function($scope, $state) {
-	// $scope.test = 'hello';
 })
 .controller('CategoryCtrl', function($scope, $state, $http) {
 	$http.get('/categories.json')
 	.success(function(response) {
 
 		$scope.categories = response;
-		console.log(response);
+		// console.log(response);
 
-		// $scope.categories = [
-		// 	{
-		// 	name: 'student',
-		// 	issues: {
-		// 			name: 'time-management',
-		// 			name: 'learning curve'
-		// 		}
-		// 	},
+		$scope.categoryTitle = true;
+		$scope.issueTitle = true;
 
-		// 	{
-		// 	name: 'professional', 
-		// 	issues: {
-		// 			name: 'sexual-harrassment',
-		// 			name: 'discrimination'
-		// 		}
-		// 	},
-		// 	{
-		// 	name: 'personal' 
-		// 		// $scope.issues = {
-		// 		// 	name: 'work-life balance',
-		// 		// 	name: 'doing-it-all woman'
-		// 		// }
-		// 	}
-		// ]
-
-		console.log($scope.categories);
-
-
-		$scope.activeCategoryId = 1;
-		$scope.categoryId = 0;
 		$scope.categoryShow = true;
+		$scope.issueShow = true;
 
 	})
-	
-	$scope.categoryBtn = function() {
-		console.log('categoryBtn');
+
+	$scope.categoryBtn = function(category) {
+		// console.log(category);
 		$http.get('/categories/1/issues.json')
 		.success(function(response) {
+			$scope.categoryShow = false;
+			$scope.categoryTitle = false;
 
+			$scope.issues = [];
 
-			$scope.issues = response;
-
-			// if($scope.issues.name === 'time-management') {
-
-			// }
-
-			console.log(response);
-		});
-	}
-
-	$scope.issueBtn = function(inspirations) {
-		console.log('issueButton');
-		$http.get('/inspirations.json')
-		.success(function(response) {
-
-			console.log(response);
-			$scope.inspirations = response;
+			for(var i = 0; i<response.length; i++) {
+				if(response[i].id === category) {
+					$scope.issues.push(response[i])
+				}
+			}
 			
 		});
 	}
+
+	$scope.issueBtn = function(issue) {
+		// console.log(issue);
+		$http.get('/inspirations.json')
+		.success(function(response) {
+			$scope.issueTitle = false;
+			$scope.issueShow = false;
+
+			console.log(response);
+
+			$scope.inspirations = [];
+
+			for(var i = 0; i<response.length; i++) {
+				if(response[i].id === issue) {
+					$scope.inspirations.push(response[i])
+				}
+			}
+
+		});
+	}
 })
-
-	// console.log($scope.issueBtn);
-
-		// console.log(categories);
-		// $scope.categories = categories;
-
-		// console.log($scope.categories);
-
-	// $scope.student1 = true;
-	// $scope.issueOptions = false;
-
-	// $scope.studentBtn = function() {
-	// 	return $scope.issueOptions = true;
-	// }
-
-	// $scope.studentBtn = function(id) {
-	// 	// console.log(id);
-
-	// 	var promise = $http.get('http://localhost:3000/categories')
-	// 	.success(function(response) {
-
-	// 		$scope.inspiration = [];
-		// 	for(var i=0; i<response.length; i++) {
-
-		// 		console.log(i);
-		// 		console.log(response[i]);
-		// 		console.log(response[i].image);
-		// 		console.log(response[i].name);
-		// 		console.log(response[i].quote);
-
-		// 			if(response[i].image && response[i].name && response[i].quote) {
-		// 				$scope.inspiration.push(response[i]);
-		// 				console.log($scope.inspiration);
-		// 			}
-		// 	}
-
-		// 	console.log(response);
-		// })
-
-		// .error(function(err) {
-		// 	console.log(err);
-	// 	})
-
-	// }
-// issue1();
-	
-
-// .controller('SettingsCtrl', function($scope, $state) {
-// 	// $scope.test = 'hello';
-// 	// $scope.goHome = function() {
-// 	// 	console.log('goHome');
-// 	// 	$state.go('home');
-// 	// }
-// })
-// 
