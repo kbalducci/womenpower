2f568971d1f4f9889e5187907ae9a595f96c30b3
Kate <kbalducci@hotmail.com>
2015-03-01 14:02:31 -0600

    added status errors to controllers

--- app/controllers/issues_controller.rb
+++ app/controllers/issues_controller.rb
@@ -2,6 +2,8 @@ class IssuesController < ApplicationController
 
   def index
     render json: Issue.all
+    rescue StandardError => e
+    render json: { message: e.message, status: 404 }.to_json, status: 404
   end
 
   private
