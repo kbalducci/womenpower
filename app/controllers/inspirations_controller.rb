000f8d592716cbde53dad85130708f98676a4e51
Kate <kbalducci@hotmail.com>
2015-03-02 00:27:10 -0600

    updated testing for devise

--- app/controllers/inspirations_controller.rb
+++ app/controllers/inspirations_controller.rb
@@ -6,20 +6,21 @@ class InspirationsController < ApplicationController
       render json: { message: e.message, status: 404 }.to_json, status: 404
   end
 
-  def create
-  @inspiration = Inspiration.new(user_params)
+#add when ready to create by users
+  # def create
+  # @inspiration = Inspiration.new(user_params)
 
-    respond_to do |format|
-      if @inspiration.save
-          format.json { render json: @inspiration, status: :created, location: @inspiration }
-      else
-          format.json { render json: @inspiration.errors, status: :unprocessable_entity }
-      end
-    end
-  end
+  #   respond_to do |format|
+  #     if @inspiration.save
+  #         format.json { render json: @inspiration, status: :created, location: @inspiration }
+  #     else
+  #         format.json { render json: @inspiration.errors, status: :unprocessable_entity }
+  #     end
+  #   end
+  # end
 
-  private
-    def inspiration_params
-      params.require(:inspiration).permit(:name, :picture, :quote)
-    end
+  # private
+  #   def inspiration_params
+  #     params.require(:inspiration).permit(:name, :picture, :quote)
+  #   end
 end
